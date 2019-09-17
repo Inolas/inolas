82274cf7e5017bdad95e2cba0031d92c86bedf5e
Inolas <salonibuddhadeo@gmail.com>
2019-09-17 01:37:29 -0700

    Social Media component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Social from './SocialMedia'
 
 export default class Sidebar extends Component {
   render() {
@@ -9,38 +10,40 @@ export default class Sidebar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
           <a className="navbar-brand js-scroll-trigger" href="#page-top">
-      <span className="d-block d-lg-none">Saloni Buddhadeo</span>
-      <span className="d-none d-lg-block">
-        <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="images/profile.jpg" alt=""></img>
-        </span>
-    </a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link js-scroll-trigger active" href="#about">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link js-scroll-trigger" href="#experience">Experience</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link js-scroll-trigger" href="#education">Education</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link js-scroll-trigger" href="#skills">Skills</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link js-scroll-trigger" href="#interests">Interests</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link js-scroll-trigger" href="#awards">Awards</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+          <span className="d-block d-lg-none">Saloni Buddhadeo</span>
+          <span className="d-none d-lg-block">
+            <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="images/profile.jpg" alt=""></img>
+            </span>
+        </a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger active" href="#about">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#experience">Experience</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#education">Education</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#skills">Skills</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#interests">Interests</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#awards">Awards</a>
+            </li>
+          </ul>
+        </div>
+        Hide me when screen size small
+        <Social></Social>
+      </nav>
     </div>
     )
   }
---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+export default class Social extends Component{
+    render(){
+        return(
+            <div className="social-icons mb-5">
+            <span className="linkedin-icon">
+              <a className="linkedin-icon" href="https://www.linkedin.com/in/saloni-buddhadeo-b94a67115/">
+                <i className="fab fa-linkedin-in"></i>
+              </a>
+            </span>
+            <span className="github-icon">
+              <a href="https://www.github.com/Inolas">
+                <i className="fab fa-github"></i>
+              </a>
+            </span>
+            </div>
+        )
+    }
+}
