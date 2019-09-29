d3fe33124767e4979ca6fe32cd2f2ddf513ddbac
Inolas <salonibuddhadeo@gmail.com>
2019-09-28 22:32:02 -0700

    profile2.png

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,7 +18,7 @@ export default class Sidebar extends Component {
           </button>
           <a className="navbar-brand js-scroll-trigger" href="#page-top">
             <span className="d-none d-lg-block">
-              <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="images/profile2.jpg" alt=""></img>
+              <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="images/profile2.png" alt=""></img>
             </span>
             <span className="d-block d-lg-none">INOLAS</span>
           </a>
